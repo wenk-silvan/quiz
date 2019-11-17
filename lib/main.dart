31fe8f9cb2d399653a3b87714823d4a3e665e7e0
silvanwenk <silvan.wenk@gmail.com>
2019-11-17 14:31:54 +0100

    Add basic button handling

--- lib/main.dart
+++ lib/main.dart
@@ -3,14 +3,41 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Answer chosen!');
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favorite color?',
+      'What\'s you favoritte animal?'
+    ];
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Text('This is my default text'),
+        body: Column(
+          children: <Widget>[
+            Text('The questions!'),
+            RaisedButton(
+              child: Text('Answer 1'),
+              onPressed: this.answerQuestion,
+            ),
+            RaisedButton(
+              child: Text('Answer 2'),
+              onPressed: () => print('Answer 2 chosen!'),
+            ),
+            RaisedButton(
+              child: Text('Answer 3'),
+              onPressed: () {
+                print('Answer 3 chosen!');
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
